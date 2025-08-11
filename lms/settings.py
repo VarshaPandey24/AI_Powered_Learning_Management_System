0544ae878893df9ee9242d0ef65d9c479f7f6933
Varsha Pandey <146333488+VarshaPandey24@users.noreply.github.com>
2025-08-11 12:12:26 +0530

    Update settings.py

--- lms/settings.py
+++ lms/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://ai-powered-learning-management-system.onrender.com']
+ALLOWED_HOSTS = ['ai-powered-learning-management-system.onrender.com']
 
 
 # Application definition
